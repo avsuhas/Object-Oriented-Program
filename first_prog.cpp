701e90188fa1f3d0d08ac759fa702b7d9ed74807
Suhas V Arudi <avsuhas@gmail.com>
2016-12-31 19:54:31 -0500

    Create first_prog.cpp

---
+++ first_prog.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+// This is my first C++ Program :-)
+
+int main( )
+{
+	int x = 5;
+	cout << "x = " << x << endl;
+
+	double y;
+	cout << "Enter a double value\n";
+	cin >> y;
+	cout << y << '\n';
+
+	const char *Str = "Its Object Oriented program";
+	cout << Str << endl;
+
+	return 0;
+}
