f2f896bb4b633f10a42cd521edd7d1742e804a92
Suhas V Arudi <avsuhas@gmail.com>
2017-01-08 16:28:24 -0500

    Create CopyConstructorUserDefined.cpp

---
+++ CopyConstructorUserDefined.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Example
+{
+	private:
+		int Data1;
+		int Data2;
+	public:
+		Example(int x)
+		{
+			Data1 = x;
+			Data2 = ++x;	
+		}	
+
+		Example( Example &e )
+		{
+			Data1 = e.Data1;
+			Data2 = e.Data2;
+		}
+		void GetVal( )
+		{
+			cout << "Data1 = " << Data1 << " Data2 = " << Data2 << endl;
+		}
+};
+
+int main( )
+{
+	Example e1(5);
+	e1.GetVal( );
+
+	Example e2(e1);
+	e2.GetVal( );
+
+}
