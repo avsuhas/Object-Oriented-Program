c90397995f521024fa6b74abcf64da412651ebc3
Suhas V Arudi <avsuhas@gmail.com>
2017-01-17 23:11:04 -0500

    Create namespace1.cpp

---
+++ Namespace/namespace1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+namespace Subhash 
+{
+	int i;
+	void function( )
+	{	
+		cout << "Hello function\n";
+	}
+}
+
+int main( )
+{
+	i = 6;
+	function( );
+	return 0;
+}
