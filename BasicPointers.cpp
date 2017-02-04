b39e974ff77f5b0223e56a3173440910095d3aa3
Suhas V Arudi <avsuhas@gmail.com>
2017-02-03 21:53:02 -0500

    Add files via upload

---
+++ BasicPointers.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+int main( )
+{	
+	int a = 6;
+
+	int *p = &a;
+	cout << a << << p << *p << endl;
+
+	int **q = &p
+	cout << a << p << q << *p << *q << **q << endl; 
+		
+}
