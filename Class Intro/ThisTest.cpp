d28491017d9cc31329bc78a9e5ffc89dcb9f4f49
Suhas V Arudi <avsuhas@gmail.com>
2017-02-14 21:55:30 -0500

    Create ThisTest.cpp

---
+++ Class Intro/ThisTest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+class ThisTest
+{
+	private:
+		
+	public:
+		void Display( )
+		{
+			cout << "this addess is = " << this << endl;
+		}
+};
+
+
+int main( )
+{
+	ThisTest t;
+	cout << "Address of t   = " << &t  << endl;
+	t.Display( );
+
+	return 0;
+}
