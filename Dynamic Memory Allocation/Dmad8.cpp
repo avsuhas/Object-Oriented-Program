07a7f3f13e9daab2f903914bc96e403ce0c5038c
Suhas V Arudi <avsuhas@gmail.com>
2017-02-22 08:31:51 -0500

    Create Dmad8.cpp

---
+++ Dynamic Memory Allocation/Dmad8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <new>
+
+using namespace std;
+const int size = 10;
+void CatchHandler( );
+int Allocations;
+int main( )
+{
+	set_new_handler(CatchHandler);
+	int *Ptr;
+	try
+	{
+		for( int i = 0; i < 100000; i++ )
+		{
+			Ptr = new int[10000000000];
+			++Allocations;
+			cout << Ptr << endl;
+		}
+	}
+	catch ( bad_alloc )
+	{
+		cout << "Baddddy\n";
+	}
+	return 0;
+}
+
+void CatchHandler( )
+{
+	cout << "I am caught" << endl;
+	cout << "Cannot allocate memory after " << Allocations << " allocations\n";
+	exit(5);
+}
