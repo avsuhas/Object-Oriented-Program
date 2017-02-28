23ce6aa8c09069d2d7a752451d22d596fd512fbd
Suhas V Arudi <avsuhas@gmail.com>
2017-02-27 22:58:56 -0500

    Create Dmad12.cpp

---
+++ Dynamic Memory Allocation/Dmad12.cpp
@@ -0,0 +1,32 @@
+
+/* DEMONSTRATES SHALLOW COPY*/
+#include <iostream>
+using namespace std;
+
+class ShallowCopy
+{
+	private:
+		char *Str;
+	public:
+		ShallowCopy( const char *String )
+		{	
+			Str = new char [10];
+			strcpy( Str, String);
+		}
+		void Display( )
+		{
+			cout << (int *)Str << endl;
+			cout << Str << endl;
+		}
+};
+
+int main( )
+{
+	ShallowCopy Sc1( "Subhash" );
+	ShallowCopy Sc2(Sc1); 
+	
+	Sc1.Display( );
+	Sc2.Display( );
+	
+	return 0;
+}
